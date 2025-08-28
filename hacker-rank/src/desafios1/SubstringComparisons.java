6154748a92cf5f53f5b2a7ef1bd215f80649e9ab
GuilhermeSerafim <guilerstudies@gmail.com>
2025-08-28 15:21:39 -0300

    Finalizando desafio substring comparisions

--- hacker-rank/src/desafios1/SubstringComparisons.java
+++ hacker-rank/src/desafios1/SubstringComparisons.java
@@ -1,25 +1,24 @@
 package desafios1;
 
+import java.util.Arrays;
+
 public class SubstringComparisons {
 
     public static void main(String[] args) {
-        // System.out.println(getSmallestAndLargest("welcometojava", 3));
-        getSmallestAndLargest("welcometojava", 3);
+        System.out.println(getSmallestAndLargest("welcometojava", 3));
     }
 
-    public static void getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
+    public static String getSmallestAndLargest(String s, int k) {
         // Quantidade de combinações possiveis == s.lenght() - k + 1
         String combinacoes[] = new String[s.length() - k + 1];
 
+        // Primeiro looping para popular as combinações possíveis
         for (int i = 0; i < combinacoes.length; i++) {
-            combinacoes[i] = s.substring(i, i + 2);
+            combinacoes[i] = s.substring(i, i + 3);
         }
 
-        System.out.println(combinacoes.toString());
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-        // return smallest + "\n" + largest;
+        Arrays.sort(combinacoes);
+
+        return combinacoes[0] + "\n" + combinacoes[combinacoes.length - 1];
     }
 }
