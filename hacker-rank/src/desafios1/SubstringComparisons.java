8cc62bd9b79bd83181e60c760139331e1c013b43
GuilhermeSerafim <guilerstudies@gmail.com>
2025-08-28 16:43:23 -0300

    Correção do challenger e aplicando testes - tudo ok

--- hacker-rank/src/desafios1/SubstringComparisons.java
+++ hacker-rank/src/desafios1/SubstringComparisons.java
@@ -3,7 +3,42 @@ package desafios1;
 public class SubstringComparisons {
 
     public static void main(String[] args) {
+        System.out.println("Case 1");
         System.out.println(getSmallestAndLargest("welcometojava", 3));
+        // esperado:
+        // ava
+        // wel
+
+        // System.out.println("Case 2");
+        System.out.println(getSmallestAndLargest("abc", 2));
+        // getSmallestAndLargest("abc", 2);
+        // esperado:
+        // ab
+        // bc
+
+        // System.out.println("Case 3");
+        System.out.println(getSmallestAndLargest("zzzz", 2));
+        // // esperado:
+        // // zz
+        // // zz
+
+        // System.out.println("Case 4");
+        System.out.println(getSmallestAndLargest("AbC", 2));
+        // // esperado:
+        // // Ab
+        // // bC
+
+        // System.out.println("Case 5");
+        System.out.println(getSmallestAndLargest("programming", 4));
+        // // esperado:
+        // // ammi
+        // // rogr
+
+        // System.out.println("Case 6");
+        System.out.println(getSmallestAndLargest("hi", 2));
+        // // esperado:
+        // // hi
+        // // hi
     }
 
     public static String getSmallestAndLargest(String s, int k) {
@@ -13,7 +48,10 @@ public class SubstringComparisons {
 
         // Primeiro looping para popular as combinações possíveis
         for (int i = 0; i < combinacoes.length; i++) {
-            combinacoes[i] = s.substring(i, i + 3);
+            // Tem que mudar aqui
+            // combinacoes[i] = s.substring(i, i + 3);
+            combinacoes[i] = s.substring(i, i + k);
+
         }
 
         // Ordenação Bubble Sort
