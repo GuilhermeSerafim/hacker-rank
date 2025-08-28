0aec09d3be05fdb16df5ed9ca75ac7b9af3cd00b
GuilhermeSerafim <guilerstudies@gmail.com>
2025-08-28 15:29:17 -0300

    fazendo ordenação com bubble sort

--- hacker-rank/src/desafios1/SubstringComparisons.java
+++ hacker-rank/src/desafios1/SubstringComparisons.java
@@ -1,7 +1,5 @@
 package desafios1;
 
-import java.util.Arrays;
-
 public class SubstringComparisons {
 
     public static void main(String[] args) {
@@ -11,13 +9,23 @@ public class SubstringComparisons {
     public static String getSmallestAndLargest(String s, int k) {
         // Quantidade de combinações possiveis == s.lenght() - k + 1
         String combinacoes[] = new String[s.length() - k + 1];
+        String aux = "";
 
         // Primeiro looping para popular as combinações possíveis
         for (int i = 0; i < combinacoes.length; i++) {
             combinacoes[i] = s.substring(i, i + 3);
         }
 
-        Arrays.sort(combinacoes);
+        // Ordenação Bubble Sort
+        for (int i = 0; i < combinacoes.length; i++) {
+            for (int j = 0; j < combinacoes.length - 1; j++) {
+                if (combinacoes[j].compareTo(combinacoes[j + 1]) > 0) {
+                    aux = combinacoes[j];
+                    combinacoes[j] = combinacoes[j + 1];
+                    combinacoes[j + 1] = aux;
+                }
+            }
+        }
 
         return combinacoes[0] + "\n" + combinacoes[combinacoes.length - 1];
     }
